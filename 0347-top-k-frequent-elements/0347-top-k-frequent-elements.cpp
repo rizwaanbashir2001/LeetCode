c7b16ef5888e04023ff2e9e6a7fd5d5381851c04
Rizwaan Bashir <rizwaanbashir2001@gmail.com>
2023-08-15 00:28:05 +0530

    Time: 19 ms (45.13%), Space: 13.8 MB (22.57%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,15 +5,29 @@ public:
         for(auto x:nums){
             mp[x]++;
         }
-        vector<pair<int,int>> vec;
-        for(auto x:mp){
-            vec.push_back({x.second,x.first});
-        }
-        sort(vec.begin(),vec.end(),greater<pair<int,int>>());
+        // vector<pair<int,int>> vec;
+        // for(auto x:mp){
+        //     vec.push_back({x.second,x.first});
+        // }
+        // sort(vec.begin(),vec.end(),greater<pair<int,int>>());
+        // vector<int> ans;
+        // for(int i=0;i<k;i++){
+        //     ans.push_back(vec[i].second);
+        // }       
+        // return ans;
+        priority_queue<
+        pair<int, int>,
+        vector<pair<int, int>>>
+    pq;
+    for (auto x:mp){
+        pq.push({x.second, x.first});
+    }
         vector<int> ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(vec[i].second);
-        }       
+        while(k--){
+            auto val= pq.top();
+            ans.push_back(val.second);
+            pq.pop();
+        }
         return ans;
     }
 };
